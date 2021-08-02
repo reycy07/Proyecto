447f1c915c095826cff805677773d2964320224e
ImperialReycy <reycristiancy@gmail.com>
2021-08-01 21:47:57 -0500

    se termino de agregar la validacion para usuarios para modificar las entradas

--- tickets.php
+++ tickets.php
@@ -1,7 +1,8 @@
 <?php require_once 'includes/header.php'?>
 
 <?php
- $ticket_current= ticketPage($conexion, $_GET['id']);
+
+$ticket_current= ticketPage($conexion, $_GET['id']);
 
  if(empty($ticket_current)){
      header("location:index.php");
@@ -17,14 +18,15 @@
             <a href="category.php?id=<?= $ticket_current['categoria_id'] ?>">
                 <h2><?= $ticket_current['categoria'] ?></h2>
             </a>
-            <h4><?= $ticket_current['fecha'] ?> |?></h4>
-
+            <h4><?= $ticket_current['fecha'] ?> | <?= $ticket_current['usuario'] ?> </h4>
             <p>
                 <?= $ticket_current['descripcion'] ?>
             </p>
             <?php if(isset($_SESSION['user']) && $_SESSION['user']['id']== $ticket_current['usuario_id']): ?>
-            <a href="edit-ticket.php" class="button">Editar ticket</a>
-            <a href="delete-ticket.php" class="button">Borrar Ticket</a>
+            <div class="box-buttom size-buttom">
+                <a href="edit-ticket.php" class="button">Editar ticket</a>
+                <a href="delete-ticket.php" class="button">Borrar Ticket</a>
+            </div>
             <?php endif; ?>
             
         </main>
